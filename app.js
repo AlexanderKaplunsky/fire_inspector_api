48b74f4ff8e72ecfea5e733d2df5e0d772f081f0
Alexander Kaplunsky <sashakaplunsky@gmail.com>
2020-04-16 17:29:39 +0300

    fix

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express'),
   app = express();
 const { PORT } = require('./config/env');
 const router = require('./controllers');
+var cors = require('cors')
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cors())
 app.use(bodyParser.json());
 app.use(router);
+
 
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT} port`);
